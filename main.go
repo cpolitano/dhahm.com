9d9fee06f6336f8111c4658db7a8711f6fc17b90
Clare Hutchings <clareh@socialtables.com>
2017-02-07 22:21:04 -0500

    about and listen pages

--- main.go
+++ main.go
@@ -9,11 +9,13 @@ import (
 var tpl *template.Template
 
 func init() {
-	tpl = template.Must(template.ParseFiles("templates/index.gohtml"))
+	tpl = template.Must(template.ParseGlob("templates/*"))
 }
 
 func main() {
 	http.HandleFunc("/", index)
+	http.HandleFunc("/about", about)
+	http.HandleFunc("/listen", listen)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("./public"))))
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -21,6 +23,20 @@ func main() {
 
 func index(w http.ResponseWriter, req *http.Request) {
 	err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func about(w http.ResponseWriter, req *http.Request) {
+	err := tpl.ExecuteTemplate(w, "about.gohtml", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func listen(w http.ResponseWriter, req *http.Request) {
+	err := tpl.ExecuteTemplate(w, "listen.gohtml", nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
